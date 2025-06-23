d3cbf9a314a87dfd069f0ed48fdeab8791977843
prateekbanra7 <66116227+prateekbanra7@users.noreply.github.com>
2025-06-23 10:16:16 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,58 @@
+package in.abc.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer sid;
+	private String sname;
+	private String saddr;
+	private Integer sage;
+
+	public Student() {
+		System.out.println("Hibernate uses Zero argument constructor internally");
+	}
+
+	public Integer getSid() {
+		return sid;
+	}
+
+	public void setSid(Integer sid) {
+		this.sid = sid;
+	}
+
+	public String getSname() {
+		return sname;
+	}
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+
+	public String getSaddr() {
+		return saddr;
+	}
+
+	public void setSaddr(String saddr) {
+		this.saddr = saddr;
+	}
+
+	public Integer getSage() {
+		return sage;
+	}
+
+	public void setSage(Integer sage) {
+		this.sage = sage;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sname=" + sname + ", saddr=" + saddr + ", sage=" + sage + "]";
+	}
+}
